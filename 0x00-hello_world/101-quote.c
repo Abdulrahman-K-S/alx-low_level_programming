de6c69417cc07c8a9120ba3ef93414307f1ad7be
Abdulrahman Khaled <21-101079@students.eui.edu.eg>
2023-07-13 16:34:06 -0700

    Added 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: Prints a string without the use of
+ * printf and puts
+ *
+ * Return: 1 if succsesful
+*/
+
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
