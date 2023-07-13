9892b7cc7d859993886fe24d1556a30764649b17
Abdulrahman Khaled <21-101079@students.eui.edu.eg>
2023-07-13 16:12:55 -0700

    Added 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A c program that prints a string
+ *
+ * Return: 0 which indicates a success
+*/
+
+int main(void)
+{
+	printf("%s", "with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
