761e13202f48f22ac0fcab3f3ddd21f84ef1f59c
Abdulrahman Khaled <21-101079@students.eui.edu.eg>
2023-07-14 14:44:59 -0700

    Added 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: C program that prints from 0 to 10 (exclusive)
+ *
+ * Return: (0) which indicates a success
+*/
+
+int main(void)
+{
+	for (int i = 0; i < 10; i++)
+		printf("%d", i);
+
+	return (0);
+}
