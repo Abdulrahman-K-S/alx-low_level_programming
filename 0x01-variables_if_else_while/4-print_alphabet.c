022501b848e710df8d58b409230ea58b6af886a3
Abdulrahman Khaled <21-101079@students.eui.edu.eg>
2023-07-14 06:33:39 -0700

    Added 4-print_alphabet.c

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: C program that prints out the alphabet
+ * using putchar() function leaving behind 'q' & 'e'
+ *
+ * Return: (0) which indicates a success
+*/
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch != 'q' || ch != 'e')
+			putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
