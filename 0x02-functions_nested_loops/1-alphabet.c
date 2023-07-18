4b52d626ff85e93038a7fbe6bb73906a8e1ec5ee
Abdulrahman Khaled <21-101079@students.eui.edu.eg>
2023-07-18 15:40:36 -0700

    Added '1-alphabet' & '2-alphabet_x10'

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - A function that utilizes putchar()
+ * to print the alphabet.
+*/
+
+void print_alphabet(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+}
