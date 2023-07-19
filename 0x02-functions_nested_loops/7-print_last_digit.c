0c44981a3c87acd9475a192c0feccd2a6dda35b6
Abdulrahman Khaled <21-101079@students.eui.edu.eg>
2023-07-19 04:09:39 -0700

    fixed '7-print_last_digit.c'

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -14,9 +14,9 @@ int print_last_digit(int n)
 	int last_digit;
 
 	if (n < 0)
-		n *= -1;
-
-	last_digit = n % 10;
+		last_digit = -1 * (n % 10);
+	else
+		last_digit = n % 10;
 
 	_putchar('0' + last_digit);
 	return (last_digit);
