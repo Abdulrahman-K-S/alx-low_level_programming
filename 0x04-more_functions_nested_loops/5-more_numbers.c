e8c98444e5e04b70434961f152cff2229bbf25bd
Abdulrahman Khaled <21-101079@students.eui.edu.eg>
2023-07-20 18:15:15 -0700

    Fixed '5-more_numbers.c' #2
    Turned out that i was using wrong variables in it's place and when i fixed it it made an infinite loop so added another variable to house the ints to be printed

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,18 +7,19 @@
 
 void more_numbers(void)
 {
-	int num, count;
+	int num, count, x;
 
 	for (count = 0; count < 10; count++)
 	{
-		for (num = 0; num < 14; num++)
+		for (num = 0; num <= 14; num++)
 		{
+			x = num;
 			if (num > 9)
 			{
 				_putchar('1');
-				count = count % 10;
+				x = num % 10;
 			}
-			_putchar(count + '0');
+			_putchar(x + '0');
 		}
 
 		_putchar('\n');
