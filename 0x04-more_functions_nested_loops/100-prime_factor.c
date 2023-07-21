548617938d458f72fd4ecf99c39934840154b9cd
Abdulrahman Khaled <21-101079@students.eui.edu.eg>
2023-07-20 19:23:51 -0700

    Changed '100-prime_factor.c'
    Tried looking up online on how to solve this problem and found that this is the most suited one

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,66 +1,27 @@
 #include <stdio.h>
+#include <math.h>
 
 /**
- * _sqrt - Finds the square root of the input.
- *
- * @x: Input number.
- *
- * Return: Square root of x.
-*/
-
-double _sqrt(double x)
-{
-	float sqrt, tmp;
-
-	sqrt = x / 2;
-	tmp = 0;
-
-	while (sqrt != tmp)
-	{
-		tmmp = sqrt;
-		sqrt = (x / tmp + tmp) / 2;
-	}
-
-	return (sqrt);
-}
-
-/**
- * largest_prime_factor - A function that finds and prints the
- *                      largest prime factor of the parameter.
- *
- * @num: The number to find it's largest prime factor.
-*/
-
-void largest_prime_factor(long int num)
-{
-	int prmNu, largest;
-
-	while (num % 2 == 0)
-		num = num / 2;
-
-	for (prmNu = 3; prmNu <= _sqrt(num); prmNu += 2)
-	{
-		while (num % prmNu == 0)
-		{
-			num = num / prmNu;
-			largest = prmNu;
-		}
-	}
-
-	if (num > 2)
-		largest = num;
-	printf("%d\n", largest);
-}
-
-/**
- * main - Entry point
- *
- * Return: (0) -> Which indicates a success.
+  * main - Prints the largest prime factor of the number 612852475143
+  *
+  * Return: Always 0 (Success)
 */
 
 int main(void)
 {
-	larges_prime_factor(612852475143);
+	long x, maxf;
+	long number = 612852475143;
+	double square = sqrt(number);
+
+	for (x = 1; x <= square; x++)
+	{
+		if (number % x == 0)
+		{
+			maxf = number / x;
+		}
+	}
+
+	printf("%ld\n", maxf);
 
 	return (0);
 }
