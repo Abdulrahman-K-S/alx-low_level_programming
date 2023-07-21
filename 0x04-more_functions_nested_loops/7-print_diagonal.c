b714623fe95e22c177ab497012fe524d28b5dd0e
Abdulrahman Khaled <21-101079@students.eui.edu.eg>
2023-07-20 18:43:52 -0700

    Fixed '7-print_diagonal.c' #2
    Wasn't using the second for loop condition right plus the ascii of the '\' wasn't right so switched to using it's int value

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -4,18 +4,23 @@
  * print_diagonal - A function that prints '\' as many times
  *                  as the inputed n in a diagonal manner
  *
- * @n: The input for the loop
+ * @n: The number of diagonals to be placed
 */
 
 void print_diagonal(int n)
 {
 	int i, y;
 
-	for (i = 0; i < n; i++)
-	{
-		for (y = 0; y < n; y++)
-			_putchar(' ');
-		_putchar('\');
+	if (n <= 0)
 		_putchar('\n');
+	else
+	{
+		for (i = 1; i <= n; i++)
+		{
+			for (y = 1; y <= i; y++)
+				_putchar(' ');
+			_putchar(92); /*The equivelent to '\'*/
+			_putchar('\n');
+		}
 	}
 }
