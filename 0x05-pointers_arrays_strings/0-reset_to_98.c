41ec51b88d202a20c86e645d75b2e871af62c2ad
Abdulrahman Khaled <21-101079@students.eui.edu.eg>
2023-07-24 07:35:04 -0700

    Added '0-reset_to_98.c'

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/** reset_to_98 - A function that takes an integer input
+ *                and changes it to 98.
+ *
+ * @n: int Pointer
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
