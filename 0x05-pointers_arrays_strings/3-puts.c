46672668b20af2c721a8d38b343d5dbca7c38819
Abdulrahman Khaled <21-101079@students.eui.edu.eg>
2023-07-24 07:59:52 -0700

    Added '3-puts.c'

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - A function that hat prints a string,
+ *         followed by a new line, to stdout
+ *
+ * @s: char pointer
+*/
+
+void _puts(char *s)
+{
+	while (*s != '\0')
+	{
+		_putchar(*s)
+		s++;
+	}
+
+	_putchar('\n');
+}
