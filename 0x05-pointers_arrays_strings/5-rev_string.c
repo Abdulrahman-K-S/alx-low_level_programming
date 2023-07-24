b37446894f4bbde979fe3aac10419278ffad6592
Abdulrahman Khaled <21-101079@students.eui.edu.eg>
2023-07-24 08:50:10 -0700

    Added '5-rev_string.c'

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rev_string - A function that reverses a string.
+ *
+ * @s: The string to be reversed.
+*/
+
+void rev_string(char *s)
+{
+	int size = 0, rsize = 0;
+	char rev;
+
+	while (s[size])
+		size++;
+
+	while (size--)
+	{
+		rev[rsize] = s[size];
+		rsize++;
+	}
+
+	*s = rev;
+}
