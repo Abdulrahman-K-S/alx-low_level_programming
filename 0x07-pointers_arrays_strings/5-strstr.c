57fe6957bf03e74d03f5e472dd0631c0ac7984cc
Abdulrahman Khaled <21-101079@students.eui.edu.eg>
2023-07-31 16:28:38 -0700

    Added '5-strstr.c'

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * _strstr - A function that locates a substring.
+ *
+ * @haystack: Char pointer aka the array that we'll search in.
+ * @needle: Char pointer aka the array that we'll look with.
+ *
+ * Return: A pointer to the beginning of the located substring,
+ *         or NULL if the substring is not found.
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+	char *s1, *s2;
+
+	while (*haystack != NULL)
+	{
+		if (*haystack == *needle)
+		{
+			s1 = haystack;
+			s2 = needle;
+
+			while (*s1 && *s2)
+			{
+				if (*s1 != *s2)
+					break;
+
+				s1++;
+				s2++;
+			}
+
+			if (*s2 == NULL)
+				return (haystack);
+		}
+
+		haystack++;
+	}
+
+	return ('\0');
+}
