60effefb48e32b0965e4fd49d49f738ae1e28620
Abdulrahman Khaled <21-101079@students.eui.edu.eg>
2023-08-04 15:39:34 -0700

    Fixed the style in '2-args.c'

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -12,13 +12,13 @@
 
 int main(int argc, char const *argv[])
 {
-        int i = 0;
+	int  i = 0;
 
-        while (argc--)
-        {
-                printf("%s\n", argv[i]);
-                i++;
-        }
+	while (argc--)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
 
-        return (0);
+	return (0);
 }
