3bc9301c57127b583b0395b0316a556c0fd2fc19
Abdulrahman Khaled <21-101079@students.eui.edu.eg>
2023-08-04 15:11:08 -0700

    Added '0-whatsmyname.c'

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the program name followed by a new line.
+ *
+ * @argc: Int
+ * @argv: List
+ *
+ * Return: 0
+*/
+
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
