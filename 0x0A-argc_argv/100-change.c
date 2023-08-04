9d96aae54e9447604fff0458ad5fee07ec15f527
Abdulrahman Khaled <21-101079@students.eui.edu.eg>
2023-08-04 15:59:15 -0700

    Fixed '100-chamge.c' #2

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -13,27 +13,30 @@
 
 int main(int argc, char *argv[])
 {
-	if (argc != 2)
+	if (argc == 2)
+	{
+		int i, leastcents = 0, money = atoi(argv[1]);
+		int cents[] = {25, 10, 5, 2, 1};
+
+		for (i = 0; i < 5; i++)
+		{
+			if (money >= cents[i])
+			{
+				leastcents += money / cents[i];
+				money = money % cents[i];
+
+				if (money % cents[i] == 0)
+					break;
+			}
+		}
+
+		printf("%d\n", leastcents);
+	}
+	else
 	{
 		printf("Error\n");
 		return (1);
 	}
 
-	int i, leastcents = 0, money = atoi(argv[1]);
-	int cents[] = {25, 10, 5, 2, 1};
-
-	for (i = 0; i < 5; i++)
-	{
-		if (money >= cents[i]);
-		{
-			leastcents += money / cents[i];
-			money = money % cents[i];
-
-			if (money % cents[i] == 0)
-				break;
-		}
-	}
-
-	printf("%d\n", leastcents);
 	return (0);
 }
