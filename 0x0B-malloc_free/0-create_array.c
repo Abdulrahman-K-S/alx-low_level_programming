31c1539e4086577c7bd48ec55b2cef75441979b9
Abdulrahman Khaled <21-101079@students.eui.edu.eg>
2023-08-08 03:32:25 -0700

    Fixed the warnings

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,13 +15,13 @@
 
 char *create_array(unsigned int size, char c)
 {
+	char *arr;
+	unsigned int i;
+	
 	if (size <= 0)
 		return (NULL);
 
-	char *arr;
-	unsigned int i;
-
-        *arr = malloc(sizeof(char) * size);
+        arr = malloc(sizeof(char) * size);
 	if (arr == NULL)
 		return (NULL);
 
