f41cf61f59e794f0e4dc8b870dc405089ccd4126
Abdulrahman Khaled <21-101079@students.eui.edu.eg>
2023-08-10 11:40:45 -0700

    Hopefully this works

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -24,10 +24,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	concatString = malloc(sizeof(char) * (i + j) + 1);
 	if (concatString == NULL)
-	{
-		free(concatString);
-	        return (NULL);
-	}
+		return (NULL);
+
 
 	for (k = 0; k < i; k++)
 		concatString[k] = s1[k];
