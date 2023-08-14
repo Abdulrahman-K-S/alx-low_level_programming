fa0e23bb2528bfad22a79056cb8457246069fbb9
Abdulrahman Khaled <21-101079@students.eui.edu.eg>
2023-08-14 15:27:41 -0700

    Fixed the error

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,11 +16,4 @@ struct dog
 	float age;
 };
 
-/* Other Functions */
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-
-
 #endif
