b88cec8d064854e280cb9f767c06c8fd52335e6e
Abdulrahman Khaled <21-101079@students.eui.edu.eg>
2023-08-17 16:37:39 -0700

    Fixed

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -24,8 +24,8 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(ap, n);
 
 	while (i--)
-		printf("%d%s", va_arg(ap, int), separator ? separator : "");
-	printf("\n");
+		printf("%d%s", va_arg(ap, int),
+		       i ? (separator ? separator : "") : "\n");
 
 	va_end(ap);
 }
