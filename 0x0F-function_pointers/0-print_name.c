099ed6b3667b07fe579425b19a7d3fbe3f8f5be5
Abdulrahman Khaled <21-101079@students.eui.edu.eg>
2023-08-16 17:45:02 -0700

    Added '0-print_name.c'

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "functions_pointers.h"
+
+/**
+ * print_name - A function that prints a name using the
+ *              pointer function given in the parameter.
+ *
+ * @name: The name to be printed.
+ * @f: The pointer function passed.
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
