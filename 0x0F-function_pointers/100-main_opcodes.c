bc7d4129febf4085251817eb0d3e8de4f9cd69dc
Abdulrahman Khaled <21-101079@students.eui.edu.eg>
2023-08-16 19:21:46 -0700

    Added '100-main+opcodes.c'

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,29 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - The start of the programming.
+ *
+ * @argc: The number of args.
+ * @argv: Argument vecotr.
+ *
+ * Return: Always 0.
+*/
+
+int main(int argc, char **argv)
+{
+	char *p = (char *)main;
+	int b;
+
+	if (argc != 2)
+		printf("Error\n"), exit(1);
+
+	b = atoi(argv[1]);
+	if (b < 0)
+		printf("Error\n"), exit(2);
+
+	while (b--)
+		printf("%02hhx%s", *p++, b ? " " : "\n");
+	return (0);
+}
