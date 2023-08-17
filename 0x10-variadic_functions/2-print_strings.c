d986589dca3976dc2fa79cbce4efec6ce745d040
Abdulrahman Khaled <21-101079@students.eui.edu.eg>
2023-08-17 16:34:22 -0700

    Fixed

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -26,7 +26,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	while (i--)
 		printf("%s%s", ((str = va_arg(ap, char*)) ? str : "(nil)"),
-		       (i ? (separator ? separator : "") "\n"));
+		       (i ? (separator ? separator : "") : "\n"));
 
 	va_end(ap);
 }
