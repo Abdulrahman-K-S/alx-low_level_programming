8c79960b228c3045dc3f440659d2c2e816b8629d
Abdulrahman Khaled <21-101079@students.eui.edu.eg>
2023-08-28 23:36:13 -0700

    Changed approach

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,11 +10,12 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	while (head)
+	if (head)
 	{
 		temp = *head;
 		*head = (*head)->next;
 		free(temp);
+		free_listint2(head);
 	}
 
 	head = NULL;
