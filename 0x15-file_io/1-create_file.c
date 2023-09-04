8fccde18a529d1c03f6bb59a3a5d8d55fba6c769
Abdulrahman Khaled <21-101079@students.eui.edu.eg>
2023-09-04 12:37:02 -0700

    added '1-create_file.c'

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * _strlen - A function that returns the length of the string entered.
+ *
+ * @s: The string to get it's length.
+ *
+ * Return: The length of @s.
+*/
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if (!s)
+		return (0);
+
+	while (*s++)
+		i++;
+	return (i);
+}
+
+/**
+ * create_file - A function that creates a file.
+ *
+ * @filename: The name of the file to create.
+ * @text_content: The text to write in the file.
+ *
+ * Return: 1 on success otherwise 0.
+*/
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t bytes = 0, len = _strlen(text_content);
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+		return (-1);
+
+	if (len)
+		bytes = write(fd, text_content, len);
+
+	close(fd);
+	return (bytes == len ? 1 : -1);
+}
