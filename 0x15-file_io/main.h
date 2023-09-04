f3556a8e86bc036d6b43e31a50fa42a6cb332ac9
Abdulrahman Khaled <21-101079@students.eui.edu.eg>
2023-09-04 12:19:41 -0700

    Added 'main.h'

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#define READ_BUF_SIZE 1024
+
+/* Putchar */
+int _putchar(charc);
+
+/* Mandatory */
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+
+#endif
