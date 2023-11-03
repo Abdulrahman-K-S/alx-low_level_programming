5bcd9cfc2029cf738ed7a9ea2487ae743920f4d0
Abdulrahman Khaled <21-101079@students.eui.edu.eg>
2023-11-03 04:53:14 -0700

    Added '2-key_index.c'

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,14 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - A function that gives the index of a key.
+ *
+ * @key: The key to get it's index.
+ * @size: The size of the hashmap.
+ *
+ * Return: The index of the given key.
+*/
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+    return (hash_djb2(key) % size);
+}
