262fce7d73dd2a5f2a2b746701d66e5afc7cef83
Abdulrahman Khaled <21-101079@students.eui.edu.eg>
2023-11-03 05:10:47 -0700

    Added '3-hash_table_set.c'

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,41 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - A function that adds an element to the hash table.
+ *
+ * @ht: The hash table.
+ * @key: The key to search in the hash table.
+ * If the key is already in the hash table then change the value to the
+ * new value.
+ * @value: The value to place in that key.
+ *
+ * Return: 1 indicating it's a success otherwise 0.
+*/
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *node, *newNode;
+
+	if (ht == NULL || key == NULL || strcmp(key, "") == 0)
+		return (0);
+
+	index = key_index(key, ht->size);
+	node = ht->array[index];
+
+	if (node && strcmp(key, node->key) == 0)
+	{
+		free(node->value);
+		node->value = strdub(value);
+		return (1);
+	}
+
+	newNode = malloc(sizeof(hash_node_t));
+	if (!newNode)
+		return (0);
+
+	newNode->key = strdub(key);
+	newNode->value = strdub(value);
+	newNode->next = ht->array[index];
+	ht->array[index] = newNode;
+	return (1);
+}
