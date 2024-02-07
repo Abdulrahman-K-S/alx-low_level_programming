c290f6e2d51a2839aa539eff1a058629e9551e9b
Abdulrahman Khaled <AK-Salah@outlook.com>
2024-02-07 06:07:35 -0800

    Updated '0-linear.c'
    Fixed the different types comparission

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -12,14 +12,14 @@
 */
 int linear_search(int *array, size_t size, int value)
 {
-	int i = 0;
+	size_t i = 0;
 
 	if (array == NULL)
 		return (-1);
 
-	for (i; i < size; i++)
+	for (; i < size; i++)
 	{
-		printf("Value checked array[%i] = [%i]\n", i, array[i]);
+		printf("Value checked array[%li] = [%i]\n", i, array[i]);
 
 		if (array[i] == value)
 		{
